501357c90a2be31bdb1151963e918baa7a80b1fd
Leonid-Lytvynov <llskot13373@gmail.com>
2023-11-28 17:36:46 +0200

    script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
 
+// const buildNav = document.querySelector('.build-nav')
+// const buildInfo = document.querySelector('.build-info')
+
+// function reportWindowSize() {
+//    const buildNavWidth = buildNav.offsetWidth;
+//    const buildInfoWidth = buildInfo.offsetWidth;
+
+//    const calcWidth = buildNavWidth - buildInfoWidth - 50;
+//    buildInfo.style.right = calcWidth + "px";
+// }
+// reportWindowSize()
+// window.onresize = reportWindowSize;
+
 
 const buildInstall = () => {
 
@@ -7,10 +20,22 @@ const buildInstall = () => {
    const buildItemStages = document.querySelector('#stages');
    const buildItemFlats = document.querySelector('#flats');
 
+   const buildItemFloor = document.querySelector('#floor');
+   const buildItemFreeFlats = document.querySelector('#all-flats');
+   const buildItemSoldFlats = document.querySelector('#sold-flats');
+   const buildItemActionFlats = document.querySelector('#action-flats');
+   const buildItemReservationFlats = document.querySelector('#reservation-flats');
+
    buildItem.forEach(item => {
       const getItemAdress = item.getAttribute('data-adress');
       const getItemStages = item.getAttribute('data-stages');
       const getItemFlats = item.getAttribute('data-flats');
+
+      const getItemFloor = item.getAttribute('data-floor');
+      const getItemFreeFlats = item.getAttribute('data-all-flats');
+      const getItemSoldFlats = item.getAttribute('data-sold-flats');
+      const getItemActionFlats = item.getAttribute('data-action-flats');
+      const getItemReservationFlats = item.getAttribute('data-reservation-flats');
 
       if (Number(getItemFlats) === 0) {
          item.classList.add('sold');
@@ -24,16 +49,29 @@ const buildInstall = () => {
       });
 
       item.addEventListener('mouseover', () => {
-         // buildItemAdress.innerHTML = getItemAdress;
-         // buildItemStages.innerHTML = getItemStages;
-         // buildItemFlats.innerHTML = getItemFlats;
          function getAttr(item, value) {
             item.innerHTML = value;
          }
-         getAttr(buildItemAdress, getItemAdress);
-         getAttr(buildItemStages, getItemStages);
-         getAttr(buildItemFlats, getItemFlats);
+
+         if (document.querySelector('.build-1')) {
+            soldItem ? getAttr(buildItemFloor, 0) : null;
+            soldItem ? getAttr(buildItemFlats, 0) : null;
+            soldItem ? getAttr(buildItemSoldFlats, 8) : null;
+            soldItem ? getAttr(buildItemActionFlats, 0) : null;
+            soldItem ? getAttr(buildItemReservationFlats, 0) : null;
+         }
+
+         getItemAdress ? getAttr(buildItemAdress, getItemAdress) : null;
+         getItemStages ? getAttr(buildItemStages, getItemStages) : null;
+         getItemFlats ? getAttr(buildItemFlats, getItemFlats) : null;
+
+         getItemFloor ? getAttr(buildItemFloor, getItemFloor) : null;
+         getItemFreeFlats ? getAttr(buildItemFreeFlats, getItemFreeFlats) : null;
+         getItemSoldFlats ? getAttr(buildItemSoldFlats, getItemSoldFlats) : null;
+         getItemActionFlats ? getAttr(buildItemActionFlats, getItemActionFlats) : null;
+         getItemReservationFlats ? getAttr(buildItemReservationFlats, getItemReservationFlats) : null;
       })
+
       item.addEventListener('click', (event) => {
          if (item.closest('.sold')) {
             event.preventDefault();
@@ -62,11 +100,21 @@ const modalInstall = () => {
       });
    });
 
-   closeModal.forEach(item => {
-      item.addEventListener('click', () => {
-         const closeBtn = document.querySelector('.show-modal');
-         closeBtn.classList.remove('show-modal');
-      })
+   closeModal.forEach(closeBtn => {
+      closeBtn.addEventListener('click', onCloseModal)
+   });
+
+   function onCloseModal() {
+      const closeBtn = document.querySelector('.show-modal');
+      closeBtn.classList.remove('show-modal');
+   };
+
+   window.addEventListener('keydown', (event) => {
+      event.key === "Escape" ? onCloseModal() : null
+   });
+
+   window.onclick = (event) => customModal.forEach(item => {
+      event.target == item ? onCloseModal() : false;
    });
 };
 document.querySelector('.custom-modal') ? modalInstall() : null;
